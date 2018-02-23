d5c035b7512637346e7bd126bf707214459e1408
Pablo Targa <pablo.targa@gmail.com>
2018-02-23 10:27:55 +0000

    Extend success alert component to accept internal warning message

--- src/app/alerts/success-alert.component.ts
+++ src/app/alerts/success-alert.component.ts
@@ -5,8 +5,13 @@ import { Component, Input } from '@angular/core';
   template: `
     <div class="alert alert-success" role="alert" *ngIf="message">
       {{ message }}
+      <div *ngIf="warningMessage">
+        <hr/>
+        <warning-alert [message]="warningMessage"></warning-alert>
+      </div>
     </div>`
 })
 export class SuccessAlertComponent {
   @Input('message') message:string;
+  @Input('warning') warningMessage:string;
 }
